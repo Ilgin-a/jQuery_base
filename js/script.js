3b42fcf4b518ca44aeb65634525dbd94b52b073e
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-03-20 09:45:08 +0300

    callback functions

--- js/script.js
+++ js/script.js
@@ -29,7 +29,14 @@ $(function() {
       fontSize: "20px"
    }, 1000) */
 
-   $(".red-box").fadeTo(1000, 0.2);
+  /*  $(".red-box").fadeTo(1000, 0.2);
    $(".green-box").delay(1000).fadeTo(1000, 0.5);
-   $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
+   $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn(); */
+
+$(".red-box").fadeTo(1000, 0, function() {
+   $(".green-box").fadeTo(1000, 0, function() {
+      $(".blue-box").fadeTo(1000, 0);
+   });
+});
+
 });
