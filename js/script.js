b1559e678a554106d4272abf17a61bdcf50cdf6d
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-03-20 11:17:28 +0300

    signup lightbox

--- js/script.js
+++ js/script.js
@@ -33,10 +33,12 @@ $(function() {
    $(".green-box").delay(1000).fadeTo(1000, 0.5);
    $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn(); */
 
-$(".red-box").fadeTo(1000, 0, function() {
+/* $(".red-box").fadeTo(1000, 0, function() {
    $(".green-box").fadeTo(1000, 0, function() {
       $(".blue-box").fadeTo(1000, 0);
    });
-});
+}); */
+
+$(".lightbox").delay(500).fadeIn(1000);
 
 });
