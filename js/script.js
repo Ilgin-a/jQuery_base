b07b1bdfc36537824224cbab1a013394d63637c1
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-03-20 09:16:08 +0300

    slideUp and Down

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ $(function() {
    $(".green-box").fadeOut(2000);
    $(".red-box").fadeIn(1000);
    $(".red-box").fadeTo(1000, 0.5);
+   $(".blue-box").hide(1000);
+   $(".blue-box").show();
+   $(".blue-box").slideUp(2000);
+   $(".blue-box").slideDown(2000);
+  
 });
