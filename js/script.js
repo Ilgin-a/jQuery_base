85febb13b8434e4212f577ff0dd1be92c79475d4
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-03-20 09:38:58 +0300

    delay

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function() {
    // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
+ /*   $(".red-box").fadeOut(2000);
    $(".green-box").fadeOut(2000);
    $(".red-box").fadeIn(1000);
    $(".red-box").fadeTo(1000, 0.5);
@@ -27,7 +27,9 @@ $(function() {
 
    $("p").animate({
       fontSize: "20px"
-   }, 1000)
+   }, 1000) */
 
-  
+   $(".red-box").fadeTo(1000, 0.2);
+   $(".green-box").delay(1000).fadeTo(1000, 0.5);
+   $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
 });
