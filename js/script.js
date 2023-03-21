587a2b4aa82e3394408fd096dba3a3da1173e5c9
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-03-21 10:46:25 +0300

    css properties array

--- js/script.js
+++ js/script.js
@@ -71,4 +71,10 @@ console.log(redBox.css("width"));
 redBox.css("background-color", "#AA7700");
 $("p").css("font-size", "18px");
 
+
+redBox.css("width", "+=20px");
+
+var properties = $("p").css(["font-size", "line-height", "color"]);
+console.log(properties);
+
 });
