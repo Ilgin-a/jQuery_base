2b787279064119df1ca7c9665d030e516a54f591
ilginarpaci@gmail.com <88985657+Ilgin-a@users.noreply.github.com>
2023-03-21 10:36:57 +0300

    jQuery_ImageSlideShow

--- js/script.js
+++ js/script.js
@@ -39,6 +39,28 @@ $(function() {
    });
 }); */
 
-$(".lightbox").delay(500).fadeIn(1000);
+// $(".lightbox").delay(500).fadeIn(1000);
+
+/* $("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+$("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
+$("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8)"); */
+
+var galleryImage = $(".gallery").find("img").first();
+var images = [
+   "images/laptop-mobile_small.jpg",
+   "images/laptop-on-table_small.jpg",
+   "images/people-office-group-team_small.jpg"
+];
+
+var i = 0;
+setInterval(function(){
+i = (i + 1) % images.length;
+galleryImage.fadeOut(function(){
+   $(this).attr("src", images[i]);
+   $(this).fadeIn();
+});
+console.log(galleryImage.attr("src"));
+}, 2000);
+
 
 });
